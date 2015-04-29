46b7abe4c7b9a3c8309169ffa0521980d03b2272
tanc@hyxt.com <123456789>
2015-04-29 14:28:56 +0800

    修改profile

--- bbh-basic/bbh-basic-povider/src/main/java/com/bbh/basic/povider/service/UserServiceImpl.java
+++ bbh-basic/bbh-basic-povider/src/main/java/com/bbh/basic/povider/service/UserServiceImpl.java
@@ -1,16 +1,16 @@
 package com.bbh.basic.povider.service;
 
+import com.alibaba.dubbo.common.logger.Logger;
+import com.alibaba.dubbo.common.logger.LoggerFactory;
 import com.bbh.basic.api.UserService;
 import com.bbh.basic.entity.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.PropertySources;
+
 
 /**
  * Created by roman_000 on 2015/4/26.
  */
 public class UserServiceImpl implements UserService {
-    private  Logger logger= LoggerFactory.getLogger(this.getClass());
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
    // private Logger businessLogger = LoggerFactory.getLogger("business");
     //private Logger defaultLogger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
     public  UserServiceImpl(){
