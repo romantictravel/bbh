99cbde864246047bed75082ec0f9df8cfb9e1e76
romantictravel <romantictravel@live.com>
2015-05-27 22:33:53 +0800

    增加

---
+++ bbh-shop/bbh-shop-manage/src/main/java/com/bbh/shop/manage/common/rsa/RSAServiceImpl.java
@@ -0,0 +1,61 @@
+/*
+
+
+
+ */
+package com.bbh.shop.manage.common.rsa;
+
+import com.bbh.common.utils.RSAUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.security.KeyPair;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+/**
+ * Service - RSA安全
+ * 
+ * @author KCSHOP Team
+ * @version 3.0
+ */
+@Component
+public class RSAServiceImpl implements RSAService {
+
+	/** "私钥"参数名称 */
+	private static final String PRIVATE_KEY_ATTRIBUTE_NAME = "privateKey";
+
+	public RSAPublicKey generateKey(HttpServletRequest request) {
+		Assert.notNull(request);
+		KeyPair keyPair = RSAUtils.generateKeyPair();
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		HttpSession session = request.getSession();
+		session.setAttribute(PRIVATE_KEY_ATTRIBUTE_NAME, privateKey);
+		return publicKey;
+	}
+
+
+	public void removePrivateKey(HttpServletRequest request) {
+		Assert.notNull(request);
+		HttpSession session = request.getSession();
+		session.removeAttribute(PRIVATE_KEY_ATTRIBUTE_NAME);
+	}
+
+	public String decryptParameter(String name, HttpServletRequest request) {
+		Assert.notNull(request);
+		if (name != null) {
+			HttpSession session = request.getSession();
+			RSAPrivateKey privateKey = (RSAPrivateKey) session.getAttribute(PRIVATE_KEY_ATTRIBUTE_NAME);
+			String parameter = request.getParameter(name);
+			if (privateKey != null && StringUtils.isNotEmpty(parameter)) {
+				return RSAUtils.decrypt(privateKey, parameter);
+			}
+		}
+		return null;
+	}
+
+}
